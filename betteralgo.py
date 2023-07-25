895dfe4f41c1ea02a99fb2d6a50df26ea8c487b2
nahinhayat <nahinhayat@gmail.com>
2023-07-24 19:24:11 -0500

    added more functionality to streamlit app and created forecasts for future prices and signals

--- betteralgo.py
+++ betteralgo.py
@@ -12,6 +12,17 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
+from datetime import datetime, timedelta
+import hvplot.pandas
+import hvplot as hv
+hv.extension('bokeh', logo=False)
+from bokeh.plotting import figure
+from bokeh.io import show
+from bokeh.models import ColumnDataSource
+
+st.markdown("# The Ultimate Trading Tool")
+st.markdown("## Find out which stock is best for your investment and when to buy/sell ")
+
 
 #get ticker imput from user
 ticker_input = st.text_input("Enter the ticker for your stock of choice")
@@ -19,9 +30,10 @@ ticker_input = st.text_input("Enter the ticker for your stock of choice")
 #after ticker is entered by user run the algo
 if ticker_input.strip():
     #import stock close price
-        today = date.today()
+        today = datetime.now()
+        start_day = today - timedelta(days=3*365)
         starter_ticker = yf.Ticker(ticker_input)
-        asset_df = starter_ticker.history(period="1d", start="2000-01-01", end=today).drop(columns=["Open", "High","Low","Volume"])
+        asset_df = starter_ticker.history(period="1d", start= start_day, end=today).drop(columns=["Open", "High","Low","Volume"])
         asset_df.index.names = ["timestamp"]
     #create columns for SMA windows
         short_window = 4
@@ -47,12 +59,47 @@ if ticker_input.strip():
         asset_df["Portfolio Cumulative Returns"] = (1+ asset_df["Portfolio Daily Returns"]).cumprod()-1
     #calc annualized return
         annualized_return = asset_df['Portfolio Daily Returns'].mean() * 252
-    
+    #calc cumulative returns
+        cumulative_return = asset_df["Portfolio Cumulative Returns"][-1]
+    #calc annual volatility
+        annual_vol = asset_df['Portfolio Daily Returns'].std() * np.sqrt(252)
+    # Visualize the value of the total portfolio
+        total_portfolio_value = asset_df[['Portfolio Total']].hvplot(
+            line_color='lightgray',
+            ylabel='Total Portfolio Value',
+            xlabel='Date',
+            width=1000,
+            height=400)
+
+#display button to show visualization of sim portfolio
+if ticker_input.strip():
+    if st.button("Display graph of a portfolio's value using our trading strategy over last three years with this stock"):
+        st.write(hv.render(total_portfolio_value, backend='bokeh'))
+        
+
 #display button for annualized return after ticker input
 if ticker_input.strip():
-    if st.button("Display annualized return"):
+    if st.button("Display annualized return for last three years"):
         st.write((annualized_return *100))
+        
+#display button for cumulative returns after ticker input
+if ticker_input.strip():
+    if st.button("Display cumulative returns for last three years"):
+        st.write((cumulative_return *100))
+        
+#display button for annual volatility after ticker input
+if ticker_input.strip():
+    if st.button("Display annual volatility for last three years"):
+        st.write((annual_vol *100))
 
-
-
-
+if ticker_input.strip():
+    investment_input = st.text_input("How much would you like to invest?")
+    if investment_input.strip():
+        investment_amount_float = float(investment_input)
+        if st.button("Calculate your average possible returns for the next year"):
+            st.write((annualized_return *investment_amount_float))
+        if st.button("Calculate the maximum possible returns") :
+            st.write((annualized_return+annual_vol)*investment_amount_float)
+        if st.button("Calculate maximum possible losses") :
+            st.write((annualized_return-annual_vol)*investment_amount_float)
+            
